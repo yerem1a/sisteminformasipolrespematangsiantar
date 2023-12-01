9d22945ee0f73501ce7b255e2d269af11bff8e90
yerem1a <yeremiapurba14@gmail.com>
2023-12-01 16:15:32 +0700

    ga tau

---
+++ resources/views/admin/layouts/js.blade.php
@@ -0,0 +1,20 @@
+<script src="{{ asset('asset') }}/adminlte/plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<script src="{{ asset('asset') }}/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/chart.js/Chart.min.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/sparklines/sparkline.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/moment/moment.min.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="{{ asset('asset') }}/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="{{ asset('asset') }}/adminlte/dist/js/adminlte.js"></script>
+<script src="{{ asset('asset') }}/adminlte/dist/js/pages/dashboard.js"></script>
+</body>
+</html>
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,25 @@
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="{{ asset('asset') }}/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Admin</span>
+    </a>
+
+    <div class="sidebar">
+      
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          
+          <li class="nav-item">
+            <a href="/admin" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Forms 
+                <p>
+                </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </aside>
